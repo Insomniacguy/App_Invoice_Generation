dc264dd9cfdd8218d01100c9aac11aa9640190a9
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-18 07:53:55 +0530

    Add Invoice Date, moved pf.addpage(), fpdf.FPDF() inside for loop.

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import glob
 import fpdf
 import pathlib
 
-pdf = fpdf.FPDF(orientation='p', unit='mm', format='A4')
-pdf.set_auto_page_break(auto=False, margin=0
-                        )
 # glob() returns a list of file paths that match the specified pattern.
 filepaths = glob.glob("Invoices/*.xlsx")
 print(filepaths)
@@ -16,14 +13,23 @@ for filepath in filepaths:
     # print(filepath)
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     # print(df)
+    pdf = fpdf.FPDF(orientation='p', unit='mm', format='A4')
+
     filename = pathlib.Path(filepath).stem
     print(filename)
     print(type(filename))
-    invoice_num = filename.split('-')
+
     pdf.add_page()
+
+    invoice_num, date = filename.split('-')
+    # date = filename.split('-')[1]
+
     pdf.set_font("Times", size=12, style='B')
-    pdf.cell(50, 12, txt=f"Invoice number: {invoice_num[0]}")
+    pdf.cell(50, 12, txt=f"Invoice number: {invoice_num}", ln=1)
+
+    pdf.set_font("Times", size=12, style='B')
+    pdf.cell(50, 12, txt=f"Date: {date}")
+
     pdf.output(f"PDFS/{filename}.pdf")
-    # pdf.output('output')
 
 # print(filepaths)
